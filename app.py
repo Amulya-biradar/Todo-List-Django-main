1bf35e39729b0972475f6ef3e837ac8bdf7a0b22
Amulya Biradar <amulyacb14@gmail.com>
2025-04-04 19:40:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
+from flask import Flask
+import os
+import datetime
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/htop')
+def htop():
+    name = "Amulya Biradar"  
+    username = os.getenv("USER", "codespace")
+    server_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    top_output = subprocess.getoutput("top -b -n 1")
+
+    return f"""
+    <pre>
+    Name: {name}
+    Username: {username}
+    Server Time (IST): {server_time}
+
+    TOP output:
+    {top_output}
+    </pre>
+    """
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
 
